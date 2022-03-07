c49816da2fdbfcb11706768dedb9b56bcdf50d2d
github takashi-hasegawa6 <euphonium813@gmail.com>
2022-03-07 12:06:30 +0900

    top.blade.phpの編集 #3
    topページの表示項目を作成(タイトル、メモ出力枠、新規登録・ログイン・ゲストボタン)

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 <div class="container">
-    <p>top test</p>
+    <div class="container">
+        <p class="p-4 m-4 bg-white">アプリタイトル</p>
+    </div>
+    <div class="container">
+        <p class="p-4 m-4 bg-white">memo内容ランダム表示</p>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="btn-group">
+            <button type="button" class="btn bg-secondary bg-opacity-25 m-4"><a href="">新規登録</a></button>
+            <button type="button" class="btn  bg-secondary bg-opacity-25 m-4"><a href="">ログイン</a></button>
+            <button type="button" class="btn  bg-secondary bg-opacity-25 m-4"><a href="">ゲスト</a></button>
+        </div>
+    </div>
 </div>
 @endsection
